f5c02b07973e3139daeb93686ddd3ecc69c7b010
Jonatan Magnusson <jonatan.magnusson@above.se>
2019-02-01 16:06:21 +0100

    Add header

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,9 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
+            <h1>Fib Calculator Kubernetes Version!</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other PAge</Link>
+            <Link to="/otherpage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib}/>
